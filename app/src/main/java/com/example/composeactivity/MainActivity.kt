5bddc3bfd795fbf5c267f48ca95b1d2b486ae7e1
seunghoon Lee <hara9052@naver.com>
2024-02-01 23:02:26 +0900

    swith practice

--- app/src/main/java/com/example/composeactivity/MainActivity.kt
+++ app/src/main/java/com/example/composeactivity/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.composeactivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,7 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeActivityTheme {
                 // A surface container using the 'background' color from the theme
-
+setContent { CustomSwitch() }
             }
         }
     }
@@ -30,9 +34,25 @@ class MainActivity : ComponentActivity() {
 fun HelloText(message: String, fontSize: Float){
     Text(text = message,fontSize = fontSize.sp, fontWeight = FontWeight.Bold)
 }
+@Composable
+fun CustomSwitch(){
+    val checked = remember {
+        mutableStateOf(true)
+    }
+    Column {
+        Switch(checked = checked.value, onCheckedChange = {checked.value = it})
+        if (checked.value){
+            Text("Switch is On")
+        }else{
+            Text("Switch is Off")
+        }
+    }
+
+}
 
 @Preview
 @Composable
 fun DemoTextPreview(){
     HelloText(message = "hi", fontSize = 12f)
+    CustomSwitch()
 }
