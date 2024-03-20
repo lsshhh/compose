34548602c12ef5f42308ce8393a63d4c6103d8ff
seunghoon Lee <hara9052@naver.com>
2024-03-20 23:59:52 +0900

    로그인 화면 만들기

--- app/src/main/java/com/example/composeactivity/ui/theme/LoginScreen.kt
+++ app/src/main/java/com/example/composeactivity/ui/theme/LoginScreen.kt
@@ -13,10 +13,13 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,6 +29,14 @@ import com.example.composeactivity.R
 
 @Composable
 fun LoginScreen() {
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -39,11 +50,15 @@ fun LoginScreen() {
         Text(text = "Welcome to Compose", fontSize = 28.sp, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.height(4.dp))
         Text(text = "welcome to your account")
-        OutlinedTextField(value = "", onValueChange = {}, label = {
+        OutlinedTextField(value = email, onValueChange = {
+            email = it
+        }, label = {
             Text(text = "Email Address")
         })
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = {}, label = {
+        OutlinedTextField(value = password, onValueChange = {
+            password = it
+        }, label = {
             Text(text = "PassWord")
         })
         Spacer(modifier = Modifier.height(16.dp))
@@ -55,7 +70,7 @@ fun LoginScreen() {
             Text(text = "Forgot password?")
         }
         // same
-        Text(text = "Forgot password?", Modifier.clickable {  })
+        Text(text = "Forgot password?", Modifier.clickable { })
 
     }
 }
